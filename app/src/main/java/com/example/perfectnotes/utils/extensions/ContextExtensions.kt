80aadd1de38ec9700ece059c8cb183c06111cb3b
@maden77 <166928262+maden77@users.noreply.github.com>
2026-02-24 20:13:29 -0500

    Create app/src/main/java/com/example/perfectnotes/utils/extensions/ContextExtensions.kt

---
+++ app/src/main/java/com/example/perfectnotes/utils/extensions/ContextExtensions.kt
@@ -0,0 +1,12 @@
+package com.example.perfectnotes.utils.extensions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+fun Context.showLongToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
